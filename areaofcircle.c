7024ca6a554e8849060bdae9099345feaeef8203
HARSHIT CHAURASIA <43291128+HaRsHiT8081@users.noreply.github.com>
2018-11-01 13:42:14 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+void main()
+{int a,r;
+printf("enter radius of circle\n");
+scanf("%d",&r);
+a=pi*r*r;
+printf("area of circle=%d\n",a);
+getch();
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int a,b,c;
+printf("Enter two numbers\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("sum of numbers =%d\n",c);
+getch();
+}

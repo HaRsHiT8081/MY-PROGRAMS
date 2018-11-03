67a803067acdacb03b7832e27ef94cc7c5fef8da
HARSHIT CHAURASIA <43291128+HaRsHiT8081@users.noreply.github.com>
2018-11-03 13:14:58 +0530

    Add files via upload

---
+++ even_odd_bitwise.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int x;
+printf("Enter number");
+scanf("%d",&x);
+if(x&1)
+printf("Odd number");
+else
+printf("Even number");
+}
---
+++ even_odd_without_bitwise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int x;
+printf("Enter number");
+scanf("%d",&x);
+if(x/2*2==x)
+printf("Even number");
+else
+printf("Odd number");
+getch();
+}

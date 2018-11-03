41eaa104827d512b2a750a0008d1d5c67383fc61
HARSHIT CHAURASIA <43291128+HaRsHiT8081@users.noreply.github.com>
2018-11-03 21:04:13 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int x;
+printf("Enter a number");
+scanf("%d",&x);
+if(x%2)
+printf("%d is odd number",x);
+else
+printf("%d is even number",x);
+getch();
+}
---
+++ greater2no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int x,y;
+printf("Enter two numbers");
+scanf("%d%d",&x,&y);
+if(x>y)
+printf("%d is greater number",x);
+elseif
+printf("%d is greater number",y);
+else
+printf("numbers are equal")
+getch();
+}

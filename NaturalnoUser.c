2ddca9d70ad7bb0b4811a6fe2512e52f74bf2fbb
HARSHIT CHAURASIA <43291128+HaRsHiT8081@users.noreply.github.com>
2018-11-05 19:56:58 +0530

    Add files via upload

---
+++ NaturalnoUser.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{int x,i;
+printf("\nEnter a natural number");
+scanf("%d",&x);
+{for(i=1;i<=x;i++)
+printf("%d ",i);
+}
+return 0;
+}
---
+++ reverseno.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{int x,i;
+printf("\nEnter a natural no");
+scanf("%d",&x);
+{for(i=x;i>=1;i--)
+printf("%d ",i);
+}
+return 0;
+}
